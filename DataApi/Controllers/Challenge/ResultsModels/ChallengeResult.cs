da4fc12391fd6a851fbe44d52a3981869af534da
Vladislav Kalashnikov-Travin <git.inayelle.arts@gmail.com>
2019-04-25 02:42:10 +0300

    add redirect

---
+++ DataApi/Controllers/Challenge/ResultsModels/ChallengeResult.cs
@@ -0,0 +1,9 @@
+using Quorum.Shared.Interfaces;
+
+namespace Quorum.DataApi.Controllers.Challenge.ResultsModels
+{
+    public class ChallengeResultModel : IDataTransferObject
+    {
+		public int Id { get; set; }
+    }
+}
